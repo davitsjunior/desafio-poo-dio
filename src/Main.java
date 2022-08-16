86afd824a7ddfa9fd7a56e20c860788f744eba4f
Davi TS Junior <dv_junior@hotmail.com>
2022-08-15 21:04:03 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,34 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDavi = new Dev();
+        devDavi.setNome("Davi");
+        devDavi.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Davi: " + devDavi.getConteudosInscritos());
+        devDavi.progredir();
+        System.out.println("Conteúdos Concluidos Davi: " + devDavi.getConteudosConcluidos());
+        System.out.println("XP..: " + devDavi.calcularTotalXp());
+
+        Dev devJunior = new Dev();
+        devJunior.setNome("Junior");
+        devJunior.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Junior: " + devJunior.getConteudosInscritos());
+        devJunior.progredir();
+        devJunior.progredir();
+        devJunior.progredir();
+        System.out.println("Conteúdos Concluídos Junior: " + devJunior.getConteudosConcluidos());
+
+        System.out.println("XP..: " + devJunior.calcularTotalXp());
 
 
     }
